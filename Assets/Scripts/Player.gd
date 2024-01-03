ce13b421fcb29bed18857134bc4d1e5ae5faf932
Gustavo <tavo.3900@gmail.com>
2024-01-03 08:16:45 -0300

    Update Player.gd

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -22,9 +22,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
-
-	
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -32,12 +29,8 @@ func _physics_process(delta):
 	if grab && Input.is_action_pressed("grab"):
 		velocity.y = 0
 		
-	
-	
-	
 	if direction:
 		velocity.x = direction * SPEED
-		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
